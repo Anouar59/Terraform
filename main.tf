a520d85745e8c1e415d660b6184d8dccba1795a9
root <anouar.d2021@gmail.com>
2024-03-24 12:25:06 +0000

    main.tf file has been added

--- main.tf
+++ main.tf
@@ -2,12 +2,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "Amazon-Linux2-Server" {
+resource "aws_instance" "Amazon-RHEL" {
   count         = 1
-  ami           = "ami-07761f3ae34c4478d"
+  ami           = "ami-0fe630eb857a6ec83"
   instance_type = "t2.medium"
   key_name      = "aws keypair"
   tags = {
-    Name = "Amazon-Linux2-VM"
+    Name = "Amazon-RHEL"
   }
+
 }
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance"  "Ubuntu" {
+  count         = 1
+  ami           = "ami-080e1f13689e07408"
+  instance_type = "t2.medium"
+  key_name      = "aws keypair"
+  tags = {
+    Name = "Ubuntu"
+   }
+
+}
+  
